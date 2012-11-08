4bf92a515ce70259db038aa859b778e0602953f4
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-08 11:03:37 +0800

    trying to make a stripe gem

--- lib/motion_stripe/stripe.rb
+++ lib/motion_stripe/stripe.rb
@@ -1,3 +1,27 @@
 module Stripe
+  def self.client(key)
+    StripeConnection.connectionWithPublishableKey(key)
+  end
   
+  def self.card(number, name, sec_code, exp_month, exp_year)
+    card =  StripeCard.alloc.init
+    card.number =       number
+    card.name =         name
+    card.securityCode = sec_code
+    card.expiryMonth =  NSNumber.numberWithInteger(exp_month)
+    card.expiryYear =   NSNumber.numberWithInteger(exp_year)
+    card
+  end
+  
+  def self.charge(stripe_client, card)
+    stripe_client.performRequestWithCard(card, success: Stripe.success, error: Stripe.error) 
+  end
+  
+  def self.error(error_respose)
+    puts error_response
+  end
+  
+  def self.success(token)
+    puts token
+  end
 end
