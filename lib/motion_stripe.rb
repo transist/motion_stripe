8f9b687be6ff5b96a93392dc8a01fe486deada15
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-08 12:30:10 +0800

    trying to remove the vendor

--- lib/motion_stripe.rb
+++ lib/motion_stripe.rb
@@ -4,7 +4,7 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'motion_stripe/*.rb')).each do |file|
     app.files.unshift(file)
   end
-  stripe_vendor = File.expand_path(File.join(File.dirname(__FILE__), '../vendor/stripe-ios'))
+  stripe_vendor = File.expand_path(File.join(File.dirname(__FILE__), '../vendor/stripe-ios/'))
   app.vendor_project(stripe_vendor, :static, :headers_dir => 'src')
 end
 
