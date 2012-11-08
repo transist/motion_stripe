7f0eab2cff244035028bfe85b557b0d7df45830e
Scott Ballantyne <ussballantyne@gmail.com>
2012-11-08 12:23:24 +0800

    trying

--- lib/motion_stripe.rb
+++ lib/motion_stripe.rb
@@ -4,8 +4,8 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'motion_stripe/*.rb')).each do |file|
     app.files.unshift(file)
   end
-  stripe_vendor = File.expand_path(File.join(File.dirname(__FILE__), '../vendor/stripe-ios/src'))
-  app.vendor_project(stripe_vendor, :static)
+  stripe_vendor = File.expand_path(File.join(File.dirname(__FILE__), '../vendor/stripe-ios'))
+  app.vendor_project(stripe_vendor, :static, :headers_dir => 'src')
 end
 
 module Stripe
